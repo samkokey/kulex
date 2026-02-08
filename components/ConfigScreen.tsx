d4e771358f75781634afcc51008712623b61499a
samkokey <06ssaammeett06@gmail.com>
2026-02-08 17:24:09 +0300

    del

--- components/ConfigScreen.tsx
+++
@@ -1,4 +0,0 @@
-// Bu bileşen artık kullanılmamaktadır. Ayarlar environment variable üzerinden alınmaktadır.
-import React from 'react';
-const ConfigScreen = () => null;
-export default ConfigScreen;
