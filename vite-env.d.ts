7502fc58e4e6349f282837ef0c3942b54bca5a7d
samkokey <06ssaammeett06@gmail.com>
2026-02-08 17:20:21 +0300

    del

--- vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_API_KEY: string;
-  readonly VITE_VIDEO_ID: string;
-  [key: string]: string | undefined;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
